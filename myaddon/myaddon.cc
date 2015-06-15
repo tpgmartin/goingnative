5d287ad5ae9c63a5a821bc908f42d84e49658494
Tom Martin <tom@tpgmartin.com>
2015-06-15 19:45:17 +0100

    Complete 'It\'s a Two-Way Street'

--- myaddon/myaddon.cc
+++ myaddon/myaddon.cc
@@ -2,14 +2,15 @@
 
 using namespace v8;
 
-NAN_METHOD(Print) {
-  Local<String> str = args[0].As<String>();
-  printf("%s\n", *String::Utf8Value(str));
-  NanReturnUndefined();
+NAN_METHOD(Length) {
+  NanScope();
+  int len = strlen(*String::Utf8Value(args[0].As<String>()));
+  Local<Number> v8len = NanNew(len);
+  NanReturnValue(v8len);
 }
 
 void Init(Handle<Object> exports) {
-  exports->Set(NanNew("print"), NanNew<FunctionTemplate>(Print)->GetFunction());
+  exports->Set(NanNew("length"), NanNew<FunctionTemplate>(Length)->GetFunction());
 }
 
 NODE_MODULE(myaddon, Init)
